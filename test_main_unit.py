82c7ea5681611c1ea9a59ec4209ce8d94ae6ba40
ne-ok <kruassankina@yandex.ru>
2025-06-22 19:17:36 +0300

    Update test_main_unit.py

--- test_main_unit.py
+++ test_main_unit.py
@@ -2,23 +2,25 @@ import unittest
 from main import calculate_average
 
 class TestCalculateAverage(unittest.TestCase):
-    
     def test_positive_numbers(self):
-        numbers = [1.5, 2.5, 3.5]
-        self.assertEqual(calculate_average(numbers), 2)  # Положительные числа
+        """Среднее арифметическое положительного списка"""
+        self.assertEqual(calculate_average([1.5, 2.5, 3.5]), 2)
 
     def test_negative_numbers(self):
-        numbers = [-1.5, -2.5, -3.5]
-        self.assertEqual(calculate_average(numbers), -3) # Отрицательные числа
+        """Среднее арифметическое отрицательного списка"""
+        self.assertEqual(calculate_average([-1.5, -2.5, -3.5]), -3)
 
     def test_mixed_numbers(self):
-        self.assertEqual(calculate_average([-1.5, 0, 1.5]), 0) # Смешанные числа
+        """Среднее арифметическое смешанного списка"""
+        self.assertEqual(calculate_average([-1.5, 0, 1.5]), 0)
 
     def test_empty_list(self):
-        self.assertIsNone(calculate_average([])) # Пустой список
-        
+        """Обработка пустого списка"""
+        self.assertIsNone(calculate_average([]))
+
     def test_single_element(self):
-        self.assertEqual(calculate_average([7.5]), 7) # Единичный элемент
+        """Список с одним числом"""
+        self.assertEqual(calculate_average([7.5]), 7)
 
 if __name__ == '__main__':
     unittest.main()
