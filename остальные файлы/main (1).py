28e1508db5c0aa93f4538d2d602a80e1021f701c
ne-ok <kruassankina@yandex.ru>
2025-06-22 20:05:42 +0300

    Add files via upload

---
+++ остальные файлы/main (1).py
@@ -0,0 +1,13 @@
+import math
+
+def calculate_average(numbers: list[float]) -> float | None:
+    """
+    Функция вычисляет среднее арифметическое чисел в списке.
+    Пустой список возвращается как None.
+    Результат округляется вниз до целого числа.
+    """
+    print("Tested by Оксана Паскида")  
+    if not numbers:
+        return None
+    avg = sum(numbers) / len(numbers)
+    return math.floor(avg)  # округляем вниз до целого
