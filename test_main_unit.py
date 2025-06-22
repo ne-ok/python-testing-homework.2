d1d474fc05d1c57b26873525b9dc01d747ffe7db
ne-ok <kruassankina@yandex.ru>
2025-06-22 19:07:39 +0300

    Update test_main_unit.py

--- test_main_unit.py
+++ test_main_unit.py
@@ -2,16 +2,23 @@ import unittest
 from main import calculate_average
 
 class TestCalculateAverage(unittest.TestCase):
+    
     def test_positive_numbers(self):
         numbers = [1.5, 2.5, 3.5]
-        self.assertEqual(calculate_average(numbers), 2)
+        self.assertEqual(calculate_average(numbers), 2)  # Положительные числа
 
     def test_negative_numbers(self):
         numbers = [-1.5, -2.5, -3.5]
-        self.assertEqual(calculate_average(numbers), -3)
+        self.assertEqual(calculate_average(numbers), -3) # Отрицательные числа
+
+    def test_mixed_numbers(self):
+        self.assertEqual(calculate_average([-1.5, 0, 1.5]), 0) # Смешанные числа
 
     def test_empty_list(self):
-        self.assertIsNone(calculate_average([]))
+        self.assertIsNone(calculate_average([])) # Пустой список
+        
+    def test_single_element(self):
+        self.assertEqual(calculate_average([7.5]), 7) # Единичный элемент
 
 if __name__ == '__main__':
     unittest.main()
